7a5f40ec22f5c7f443f68bfc05f03fd84b224060
Peacemaker Tech <muuoemmanuel649@gmail.com>
2025-05-24 23:57:34 +0300

    Update hack.js

--- plugins/hack.js
+++ plugins/hack.js
@@ -43,7 +43,7 @@ async (conn, mek, m, {
             '⚠️ *Note:* All actions are for demonstration purposes only.',
             '⚠️ *Reminder:* Ethical hacking is the only way to ensure security.',
             
-            '> *POPKID-MD-HACKING-COMPLETE ☣*'
+            '> *PEACE-MD-HACKING-COMPLETE ☣*'
         ];
 
         for (const line of steps) {
