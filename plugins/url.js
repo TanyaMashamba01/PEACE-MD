41132838b77056a4b33bdf2398412a5a03620cda
Peacemaker Tech <muuoemmanuel649@gmail.com>
2025-05-24 14:44:25 +0300

    Update url.js

--- plugins/url.js
+++ plugins/url.js
@@ -65,7 +65,7 @@ cmd({
       `*${mediaType} Uploaded Successfully*\n\n` +
       `*Size:* ${formatBytes(mediaBuffer.length)}\n` +
       `*URL:* ${mediaUrl}\n\n` +
-      `> © Uploaded by Popkid 💜`
+      `> © Uploaded by Peacemd 💜`
     );
 
   } catch (error) {
