1abdda1927bd34f7da07cac9008d17c7dc8e4d70
Peacemaker Tech <muuoemmanuel649@gmail.com>
2025-05-28 08:52:41 +0300

    Update menupeacemd.js

--- plugins/menupeacemd.js
+++ plugins/menupeacemd.js
@@ -267,7 +267,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
         // Send audio
         await conn.sendMessage(from, {
-            audio: { url: 'https://files.catbox.moe/kxdej4.m4a' },
+            audio: { url: 'https://files.catbox.moe/mq0ekb.mp3' },
             mimetype: 'audio/mp4',
             ptt: true
         }, { quoted: mek });
